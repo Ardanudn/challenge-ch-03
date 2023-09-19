d4ed1d1cbe04dce2339e3e277b9144da93bdbdae
Ardanudn <danuduhri28@gmail.com>
2023-09-19 22:46:37 +0700

    Initial Project

---
+++ app/src/main/java/com/ardanu/challenge_ch3/DetailMenuFragment.kt
@@ -0,0 +1,20 @@
+package com.ardanu.challenge_ch3
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class DetailMenuFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_menu, container, false)
+    }
+
+}
