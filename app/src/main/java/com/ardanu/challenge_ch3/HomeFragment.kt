4997d1ecfa20c882b2fc8ec86aea76901f01985e
Ardanudn <danuduhri28@gmail.com>
2023-09-22 22:09:35 +0700

    ngerapihin aja sihh

--- app/src/main/java/com/ardanu/challenge_ch3/HomeFragment.kt
+++ app/src/main/java/com/ardanu/challenge_ch3/HomeFragment.kt
@@ -14,10 +14,13 @@ import androidx.recyclerview.widget.RecyclerView
 
 class HomeFragment : Fragment() {
 
+    private var isGrid = true
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val navController = findNavController()
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
@@ -25,34 +28,14 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var isGrid = true
-
         val recyclerView = view.findViewById<RecyclerView>(R.id.rv_menu)
         val btnStyleMenu = view.findViewById<ImageButton>(R.id.btn_list)
 
-        // Inisialisasi data menu makanan
-        val menuMakanan = mutableListOf<MenuMakanan>()
-        menuMakanan.add(MenuMakanan(R.drawable.burger, "Burger", 28000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
-        menuMakanan.add(MenuMakanan(R.drawable.ayamgoreng, "Ayam Goreng", 20000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
-        menuMakanan.add(MenuMakanan(R.drawable.ayampanggang, "Ayam Panggang", 25000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
-        menuMakanan.add(MenuMakanan(R.drawable.dimsum, "Dim Sum", 27000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
-        menuMakanan.add(MenuMakanan(R.drawable.burger, "Burger1", 28000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
-        menuMakanan.add(MenuMakanan(R.drawable.ayamgoreng, "Ayam Goreng1", 20000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
-        menuMakanan.add(MenuMakanan(R.drawable.ayampanggang, "Ayam Panggang1", 25000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
-        menuMakanan.add(MenuMakanan(R.drawable.dimsum, "Dim Sum1", 27000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
-        menuMakanan.add(MenuMakanan(R.drawable.burger, "Burger2", 28000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
-        menuMakanan.add(MenuMakanan(R.drawable.ayamgoreng, "Ayam Goreng2", 20000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
-        menuMakanan.add(MenuMakanan(R.drawable.ayampanggang, "Ayam Panggang2", 25000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
-        menuMakanan.add(MenuMakanan(R.drawable.dimsum, "Dim Sum2", 27000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
-        menuMakanan.add(MenuMakanan(R.drawable.burger, "Burger3", 28000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
-        menuMakanan.add(MenuMakanan(R.drawable.ayamgoreng, "Ayam Goreng3", 20000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
-        menuMakanan.add(MenuMakanan(R.drawable.ayampanggang, "Ayam Panggang3", 25000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
-        menuMakanan.add(MenuMakanan(R.drawable.dimsum, "Dim Sum3", 27000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
+        val menu = dataMenu()
 
 
-        val navController = findNavController()
         recyclerView.layoutManager = GridLayoutManager(activity,2)
-        val menuAdapter = MenuAdapter(menuMakanan,isGrid)
+        val menuAdapter = MenuAdapter(menu,isGrid)
         recyclerView.adapter = menuAdapter
 
         btnStyleMenu.setOnClickListener {
@@ -61,16 +44,33 @@ class HomeFragment : Fragment() {
                 recyclerView.layoutManager = LinearLayoutManager(activity)
                 isGrid = false
                 // Inisialisasi adapter
-                val menuAdapter = MenuAdapter(menuMakanan, isGrid)
+                val menuAdapter = MenuAdapter(menu, isGrid)
                 recyclerView.adapter = menuAdapter
             } else {
                 btnStyleMenu.setImageResource(R.drawable.ic_grid)
                 recyclerView.layoutManager = GridLayoutManager(activity, 2)
                 isGrid = true
                 // Inisialisasi adapter
-                val menuAdapter = MenuAdapter(menuMakanan, isGrid)
+                val menuAdapter = MenuAdapter(menu, isGrid)
                 recyclerView.adapter = menuAdapter
             }
         }
+    }
+
+
+    private fun dataMenu():List<MenuMakanan> {
+        // Inisialisasi data menu makanan
+        val menuMakanan = mutableListOf<MenuMakanan>()
+        menuMakanan.add(MenuMakanan(R.drawable.burger, "Burger", 28000,"Menu cepat saji yang memiliki isian yang banyak dan mengenyangkan","Jl. Mayor Oking Citeureup Gg. Prihatin No.RT03/03, Gn. Putri, Kec. Gn. Putri, Kabupaten Bogor, Jawa Barat 16961","https://maps.app.goo.gl/8zLZfQP5pGMm2tEJ7"))
+        menuMakanan.add(MenuMakanan(R.drawable.ayamgoreng, "Ayam Goreng", 20000,"Hidangan dari ayam dengan bumbu rempah-rempah yang khas dan digoreng hingga menjadi renyah","Jl. Karang Kobar No.5, Karangkobar, Sokanegara, Kec. Purwokerto Tim., Kabupaten Banyumas, Jawa Tengah 53115","https://maps.app.goo.gl/irNJZekVWDK2hNnw9"))
+        menuMakanan.add(MenuMakanan(R.drawable.ayampanggang, "Ayam Panggang", 25000,"Hidangan dari ayam dengan lalu dipanggang atau dibakar hingga matang dan berwarna kecokelatan","Jl. Jend. Gatot Subroto No. 68D, Purwanegara, Purwokerto Utara, Brubahan, Purwanegara, Banyumas, Kabupaten Banyumas, Jawa Tengah 53116","https://maps.app.goo.gl/NTaLvgrN6hypf3Zg6"))
+        menuMakanan.add(MenuMakanan(R.drawable.dimsum, "Dim Sum", 27000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
+        for(i in 1..4){
+            menuMakanan.add(MenuMakanan(R.drawable.burger, "Burger $i", 28000,"Menu cepat saji yang memiliki isian yang banyak dan mengenyangkan","Jl. Mayor Oking Citeureup Gg. Prihatin No.RT03/03, Gn. Putri, Kec. Gn. Putri, Kabupaten Bogor, Jawa Barat 16961","https://maps.app.goo.gl/8zLZfQP5pGMm2tEJ7"))
+            menuMakanan.add(MenuMakanan(R.drawable.ayamgoreng, "Ayam Goreng $i", 20000,"Hidangan dari ayam dengan bumbu rempah-rempah yang khas dan digoreng hingga menjadi renyah","Jl. Karang Kobar No.5, Karangkobar, Sokanegara, Kec. Purwokerto Tim., Kabupaten Banyumas, Jawa Tengah 53115","https://maps.app.goo.gl/irNJZekVWDK2hNnw9"))
+            menuMakanan.add(MenuMakanan(R.drawable.ayampanggang, "Ayam Panggang $i", 25000,"Hidangan dari ayam dengan lalu dipanggang atau dibakar hingga matang dan berwarna kecokelatan","Jl. Jend. Gatot Subroto No. 68D, Purwanegara, Purwokerto Utara, Brubahan, Purwanegara, Banyumas, Kabupaten Banyumas, Jawa Tengah 53116","https://maps.app.goo.gl/NTaLvgrN6hypf3Zg6"))
+            menuMakanan.add(MenuMakanan(R.drawable.dimsum, "Dim Sum $i", 27000,"Variasi dari pangsit tradisional China yang disajikan dengan cara dikukus dan digoreng","Jl.BSD Green Office Park Jl. BSD Grand Boulevard, Sampora, BSD, Kabupaten Tangerang, Banten 15345","https://maps.app.goo.gl/h4wQKqaBuXzftGK77"))
+        }
+        return  menuMakanan
     }
 }
